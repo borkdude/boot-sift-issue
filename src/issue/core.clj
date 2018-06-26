c0218c4278e8ddcd27a0e8d5f6f7cc58607b88e8
Michiel Borkent <michielborkent@gmail.com>
2018-06-26 23:03:07 +0200

    Steps to reproduce

--- src/issue/core.clj
+++ src/issue/core.clj
@@ -1,8 +1,17 @@
+;;; start with boot dev
+;;; cider-connect on port 13337
+
+;;; evaluate this form
 (ns issue.core
   (:require [clojure.java.io :as io]))
 
+;;; evaluate this form
 (def before-edit (slurp (io/resource "issue/core.clj")))
-;;; edit the file
+
+;;; edit this file
+  
+;;; evaluate this form
 (def after-edit (slurp (io/resource "issue/core.clj")))
 
-(not= before-edit after-edit) ;; should be true, but is false in 2.8.0
+;;; evaluate this form, ;; should be true and was true in boot 2.7.2, but is false in boot 2.8.0
+(not= before-edit after-edit) 
